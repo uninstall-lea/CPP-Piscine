20ea6ed85d71caba1c65c06a2a40c7e6caaecb25
uninstall-lea <lea.bisson@gmail.com>
2023-05-04 18:08:15 +0200

    CPPo1/ex04 completed

---
+++ CPP01/ex04/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+
+void	replaceString( std::ifstream& ifs, std::ofstream& ofs,
+						std::string oldString, std::string newString ) {
+
+	size_t		pos;
+	std::string	tmp;
+
+	while (getline(ifs, tmp))
+	{
+		pos = tmp.find(oldString);
+		while (pos != std::string::npos)
+		{
+			tmp.erase(pos, oldString.length());
+			tmp.insert(pos, newString);
+			pos = tmp.find(oldString, pos + newString.length());
+		}
+		ofs << tmp << std::endl;
+	}
+}
+
+int		main (int ac, char **av) {
+
+	if (ac != 4)
+	{
+		std::cout	<< "You must enter a file name, "
+					<< "two strings, the second will replace the first\n";
+		return (1);
+	}
+
+	std::string		fileName	= av[1];
+	std::string		oldString	= av[2];
+	std::string		newString	= av[3];
+
+	std::ifstream	ifs(fileName.c_str());
+	fileName += ".replace";
+	std::ofstream	ofs(fileName.c_str());
+
+	if (!ifs.is_open() || !ofs.is_open())
+	{
+		std::cout	<< "An error occured while opening one of the files\n";
+		return (1);
+	}
+
+	replaceString(ifs, ofs, oldString, newString);
+	ifs.close();
+	ofs.close();
+
+	return (0);
+}
