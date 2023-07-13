40b3f786b00525a675a89af495c355b37d246d5b
uninstall-lea <lea.bisson@gmail.com>
2023-07-13 18:39:51 +0200

    Correct CPP00EX01

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -6,27 +6,33 @@ int main(void) {
 	std::string	userInput;
 
 	std::cout << BOLDGREEN "\nWelcome to the PhoneBook !\n" RESET << std::endl;
+	std::cout << "Please enter a command such as: ";
+	std::cout << BOLDYELLOW "'ADD', 'SEARCH' or 'EXIT'" RESET << std::endl;
 
 	while (std::cin.good())
 	{
-		std::cout << "Please enter a command such as: ";
-		std::cout << BOLDYELLOW "'ADD', 'SEARCH' or 'EXIT'" RESET << std::endl;
+
 		std::getline(std::cin, userInput);
 
 		if (userInput == "ADD")
 		{
 			if (repertoire.addContact() == FAILURE)
 				return (FAILURE);
+			std::cout << "Please enter a command such as: ";
+			std::cout << BOLDYELLOW "'ADD', 'SEARCH' or 'EXIT'" RESET << std::endl;
 		}
 
 		else if (userInput == "SEARCH")
 		{
 			if (repertoire.searchContact() == FAILURE)
 				return (FAILURE);
+			std::cout << "Please enter a command such as: ";
+			std::cout << BOLDYELLOW "'ADD', 'SEARCH' or 'EXIT'" RESET << std::endl;
 		}
 
 		else if (userInput == "EXIT")
 			return (SUCCESS);
+
 	}
 
 	return (0);
