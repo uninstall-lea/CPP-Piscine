6e19ab1427d8b47e5b9cfc2b84410570c5212e71
uninstall-lea <lea.bisson@gmail.com>
2023-11-22 16:10:15 +0100

    cpp07 fixed

--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -4,49 +4,80 @@
 #include "Array.tpp"
 
 #define MAX_VAL 750
-int	main(void)
+void	test1(void)
 {
+	std::cout << "Test 1" << std::endl;
+	Array<int> test = Array<int>(2);
+	std::cout << "size of array: " << test.size() << std::endl;
+		for (int i = 0; i < 3; i++)
+			std::cout << test[i] << std::endl;
+}
+
+void	test2(void)
+{
+	std::cout << std::endl;
+	std::cout << "Test 2" << std::endl;
+	Array<bool> test;
+	std::cout << "size of array: " << test.size() << std::endl;
+	std::cout << test[0] << std::endl;
+}
+
+void	test3(void)
+{
+	std::cout << std::endl;
+	std::cout << "Test 3" << std::endl;
+	Array<bool> test = Array<bool>(10);
 	{
-		std::cout << "TEST#1" << std::endl;
-		Array<int> intTab;
-		printArray(intTab);
-		for (unsigned int i = 0; i < intTab.size(); i++)
-			intTab[i] = 1;
-		printArray(intTab);
-		std::cout << "---------" << std::endl;
+		Array<bool> test2 = Array<bool>(test);
 	}
+}
+
+void	test4(void)
+{
+	std::cout << std::endl;
+	std::cout << "Test 4" << std::endl;
+	Array<bool> test = Array<bool>(10);
 	{
-		std::cout << "TEST#2" << std::endl;
-		Array<int> intTab(10);
-		printArray(intTab);
-		for (unsigned int i = 0; i < intTab.size(); i++)
-			intTab[i] = 1;
-		printArray(intTab);
-		std::cout << "---------" << std::endl;
+		Array<bool> test2 = test;
 	}
+	
+}
+
+int main(void)
+{
+	try
 	{
-		std::cout << "TEST#3" << std::endl;
-		Array<char> intTab(10);
-		printArray(intTab);
-		for (unsigned int i = 0; i < intTab.size(); i++)
-			intTab[i] = 'A';
-		printArray(intTab);
-		std::cout << "---------" << std::endl;
+		test1();
 	}
+	catch(const std::exception& e)
 	{
-		std::cout << "TEST#4" << std::endl;
-		Array<int> intTab(10);
-		Array<int> tab(intTab);
-		std::cout << "tab1:" << std::endl;
-		printArray(intTab);
-		std::cout << "tab2:" << std::endl;
-		printArray(tab);
-		for (unsigned int i = 0; i < intTab.size(); i++)
-			intTab[i] = 4;
-		std::cout << "tab1:" << std::endl;
-		printArray(intTab);
-		std::cout << "tab2:" << std::endl;
-		printArray(tab);
-		std::cout << "---------" << std::endl;
+		std::cerr << e.what() << '\n';
 	}
+	try
+	{
+		test2();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		test3();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		test4();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+
+	
 }
